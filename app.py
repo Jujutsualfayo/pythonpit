75d7d3669183280bf719e1fd8397aec99e1b3f59
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-02-24 07:46:35 +0000

    pit

---
+++ app.py
@@ -0,0 +1,8 @@
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = int(input("Guess: "))
+    if guess == secret_number:
+        guess += 1
+        print("You won")
