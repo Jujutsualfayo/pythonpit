b2a456e1c484651aef0b6bd6afc1ba126fd19509
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-03-06 10:31:47 +0000

    updated

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-a = 0
-b = 1
-while a <= 50:
-    print(a , end= " ")
-    a , b = b, a + b
+number = int(input("Number: "))
+factorial = 1
+i = 1
+while i <= number:
+    factorial *= i
+    i += 1
+print(f"factorial is {factorial}")
