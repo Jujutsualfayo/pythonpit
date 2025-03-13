d45c3169ac4eabd73cd2c5ef16f35ead45454504
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-03-13 13:03:32 +0000

    updated

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
-student = {"age":"25", "nationality":"Kenya", "job":"Software Engineer", "religion":"Christian"}
-for key, value in student.items():
-    print(f"{key}: {value}")
+dict1 = {"a": 1, "b": 2}
+dict2 = {"b": 3, "c": 4} 
+merged = dict1 | dict2
+print(merged)
