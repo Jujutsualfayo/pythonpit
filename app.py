83071b870b176a66bcd3c06265520324cd97b7a5
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-03-13 12:13:00 +0000

    updated

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 student = {"age":"25", "weight":"50kg", "school":"Alliance school"}
-print(student.get("work"))
+print(student.get("age"))
 student["age"] = 26
+print(student.get("age"))
+if "age" in student:
+    print("Exists")
