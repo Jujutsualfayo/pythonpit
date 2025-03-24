253a613b71e6b155e043afa26261fa5ec4d13f07
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-03-24 13:03:48 +0000

    updated

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-num = int(input("Input A no: "))
-num1 =int(input("Input B no: "))
-
-def magic_calculation(a, b):
-    return(98 + a** b)
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number > 0:
+    print("Is positive")
+elif number == 0:
+    print("is zero")
+else:
+    print("is negative")
