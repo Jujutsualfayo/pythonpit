1f1786bfbc5b881706b1b71561036e73bf626b40
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-03-24 08:56:27 +0000

    updated

--- app.py
+++ app.py
@@ -1,5 +1,1 @@
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-
-print(str.index("with Python"))
-
+import this
