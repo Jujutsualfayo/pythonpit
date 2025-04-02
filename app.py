ed30380607a85c04530c6e0d0206c58774c3e536
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-04-02 11:09:46 +0000

    updated

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 def print_last_digit(number):
-    
+    last_digit = abs(number) % 10
+    print(last_digit, end="")
+    return last_digit
+number = int(input("Insert number: "))
+print_last_digit(number)
+print()
+
+
+
+
