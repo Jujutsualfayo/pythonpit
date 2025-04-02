d04d532466c1ef7cd2a20ba505565960224cedb6
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-04-02 09:14:09 +0000

    updated

--- app.py
+++ app.py
@@ -1,5 +1,2 @@
-def uppercase(str):
-    for c in str:
-        uppercase_char = chr(ord(c) -32) if "a" <= c <= "z" else c
-        print(uppercase_char, end="")
-    print()
+def print_last_digit(number):
+    
