2c94591a678a584bc1f7206fb4f62c460718d4e8
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-04-04 11:47:38 +0000

    updated

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-x = [1,2,3,4,5]
-print(dir(x))
+if __name__ == "__main__":
+    from calculator_1 import(add, sub, mul, div )
+
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a,b, sub(a, b)))
+    print("{} * {} = {}".format(a,b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
+          
