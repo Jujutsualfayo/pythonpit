9ba9454c4d69e3ca77bab0a99584bc5793475a78
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-04-07 05:50:28 +0000

    updated

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 if __name__ == "__main__":
-    from calculator_1 import(add, sub, mul, div )
+    '''File to add sum of arguments.'''
+    
+    import sys
+    total = 0
+    for i in range(len(sys.argv) -1):
+        total += int(sys.argv[i + 1])
+        print(total)
+        
 
-    a = 10
-    b = 5
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a,b, sub(a, b)))
-    print("{} * {} = {}".format(a,b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
           
