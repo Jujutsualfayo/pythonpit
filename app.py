098e126728230d03caac2a20f385a486f7c1108c
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-04-07 05:54:55 +0000

    updated

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 if __name__ == "__main__":
-    '''File to add sum of arguments.'''
-    
+    '''File that sums up the arguments decoded. '''
     import sys
     total = 0
     for i in range(len(sys.argv) -1):
         total += int(sys.argv[i + 1])
         print(total)
+
         
 
 
