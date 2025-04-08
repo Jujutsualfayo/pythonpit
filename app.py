4dd76455d15fb3543f0cb23ddd6a91d37942ed8c
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-04-08 07:10:43 +0000

    updated

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-if __name__ == "__main__":
-    '''File that sums up the arguments decoded. '''
-    import sys
-    total = 0
-    for i in range(len(sys.argv) -1):
-        total += int(sys.argv[i + 1])
-        print(total)
-
-        
-
+squares = [1,2,3,4,5]
+print(squares[1])
 
           
