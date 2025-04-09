310a09ee2e64868d77d3c0640b7b253d52515cbf
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-04-09 11:41:38 +0000

    updated

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
-def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+def print_reversed_list_integer(my_list=[]):
+    '''printing list in reverse. '''
+    if isinstance(my_list, list):
+        reversed_list = my_list[::-1]
+        for i in reversed_list:
+            print("{:d}".format(i))
