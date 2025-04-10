9aaff9fc53dc927280ca8ce93ed3c3a127fa77d6
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-04-10 09:46:51 +0000

    updated

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-matrix = [[1, 2, 3], [4, 5, 6]]
-print(matrix)
+#!/usr/bin/python3
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
