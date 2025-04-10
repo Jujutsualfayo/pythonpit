4d19acd0643a3da405ceb01266a13f96ec0059f0
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-04-10 08:33:08 +0000

    updated

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-def new_in_list(my_list, idx, element):
-    '''replace in a copy.'''
-    if idx  < 0 or idx > (len(my_list) -1):
-        return (my_list)
-
-    copy = my_list.copy
-    copy[idx] = element
-    return copy
+for i in range(1, 20):
+    even = i % 2 == 0
+    if even:
+        print(i)
