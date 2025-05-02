a87d8fc87a035dff2eeea028a442d2585a5b7ee4
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-02 09:43:21 +0000

    updated

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-def safe_print_integer_err(value):
-    try:
-        print("{:d}".format(value))
-        return True
-    except Exception as e:
-        import sys
-        print("Exception: {}".format(e), file=sys.stderr)
-        return False
+class Person:
+    pass
+
+p = Person()
+print(p)
