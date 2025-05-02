f9f07e81b498230aa2900f7556b0b871f6588379
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-02 07:11:01 +0000

    updated

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-def safe_print_list(my_list=[], x=0):
-    count = 0
+def safe_print_integer(value):
     try:
-        for i in range(x):
-            print(my_list[i], end="")
-            count += 1
-    except IndexError:
-     pass
-    return count
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
+
+
     
