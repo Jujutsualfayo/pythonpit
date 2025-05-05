e10eefa5c369fc25cddf954e9e04ceebacddf283
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-05 10:07:08 +0000

    updated

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
+    def get_size(self):
+        return self.__size
+    
 sq1 = Square(25)
 sq2 = Square(36)
-print(sq1, sq2)
+print("Square 1 size is", sq1.get_size())
+print("Square 2 size is", sq2.get_size())
