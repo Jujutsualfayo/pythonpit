701f878bf1e605bb2967200df776164710571c44
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-05 13:10:28 +0000

    updated

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 class Square:
     def __init__(self, size=0):
         self.size = size
-
+    
     @property
     def size(self):
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -13,4 +13,3 @@ class Square:
         if value < 0:
             raise ValueError("must be more than 0")
         self.__size = value
-        
