b74cd9802fc05ee94af60f2bdeb06e40d084b064
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-05 09:50:47 +0000

    updated

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-class Dog:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-dog1 = Dog('Rexx', 5)
-dog2 = Dog("Maxxine", 2)
-
-print(dog1.name, dog1.age)
-print(dog2.name, dog2.age)
+class Square:
+    def __init__(self):
+        pass
+    
