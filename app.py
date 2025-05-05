b74570e89c379cbefc2f712ca50325c268cb258d
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-05 10:24:45 +0000

    updated

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 class Square:
-    def __init__(self, size):
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("Size must be an integer")
+        if size < 0:
+            raise ValueError("Value must be more than zero")
         self.__size = size
-
-    def get_size(self):
-        return self.__size
-    
-sq1 = Square(25)
-sq2 = Square(36)
-print("Square 1 size is", sq1.get_size())
-print("Square 2 size is", sq2.get_size())
