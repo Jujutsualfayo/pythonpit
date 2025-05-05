71adf653b174f602f7ab6c7ca38b041e1758ec5f
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-05 08:01:16 +0000

    updated

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 class Dog:
-    species = "Cannis familiaris"
-
     def __init__(self, name, age):
         self.name = name
         self.age = age
-    
-print(Dog.species)
 
-dog1 = Dog("Buddy", 2)
-dog2 = Dog("Bruno", 3)
+dog1 = Dog('Rexx', 5)
+dog2 = Dog("Maxxine", 2)
 
-print(dog1.name)
-print(dog2.name)
+print(dog1.name, dog1.age)
