429b9cd85aa9e3fd08c5a516a84632c0ab63b12c
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-05 10:29:35 +0000

    updated

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ class Square:
         if size < 0:
             raise ValueError("Value must be more than zero")
         self.__size = size
+
+    def get_size(self):
+        return self.__size
+    
+sq1 = Square()
+sq2 = Square (56)
+
+print("Size of sq1 is", sq1.get_size)
+print("Size of sq2 is", sq2.get_size)
+
+    
