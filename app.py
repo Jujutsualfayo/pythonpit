243491edf4f17f858ea7873a9f5e22e89becae93
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-07 10:41:29 +0000

    updated

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
-grade = input("Enter Grade: ")
-print(f"Your grade is {grade}")
-print("Grade type is: ", type(grade))
+a = [1, 2, 3]
+b = [1, 2, 3]
+c = a
+
+print(id(a))  # Memory address of object 'a'
+print(id(b))  # Different object, same content, so different id
+print(id(c))  # Same object as 'a', so same id
