1e6f7751833cc2e1c99834d81de9e3db27e1d004
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-07 10:39:14 +0000

    updated

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-items = ['spam!', 1, ['Brie', 'Roquefort', 'Pol le Veq'], [1, 2, 3]]
-for item in items:
-    try:
-        print(len(item))
-    except TypeError:
-        print("Item has no length:", item)
+grade = input("Enter Grade: ")
+print(f"Your grade is {grade}")
+print("Grade type is: ", type(grade))
