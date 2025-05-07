a8a48fef4a63aef6c8001ae8e0f4dd2d2e4b58df
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-07 10:10:52 +0000

    updated

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
-a = [1, 2, 3]
-b = a[:]
-print(b)
+items = ['spam!', 1, ['Brie', 'Roquefort', 'Pol le Veq'], [1, 2, 3]]
+for item in items:
+    try:
+        print(len(item))
+    except TypeError:
+        print("Item has no length", item)
