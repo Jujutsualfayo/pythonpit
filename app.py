50b61285a799ded69a0ca5840fb3474d05fd7e91
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-07 12:00:12 +0000

    updated

--- app.py
+++ app.py
@@ -1,7 +1,1 @@
-a = [1, 2, 3]
-b = [1, 2, 3]
-c = a
 
-print(id(a))  # Memory address of object 'a'
-print(id(b))  # Different object, same content, so different id
-print(id(c))  # Same object as 'a', so same id
