db51a64d02a47f9456b72a6641faa0afb73e1ad1
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-08 08:14:29 +0000

    updated

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def greet(self):
+        return f"Hello my name is {self.name}"
 def lookup(obj):
-    return dir(obj)
+        return dir(obj)
+    
+p =Person("Alfayo")
+
+print(lookup(p))
