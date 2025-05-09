503dd37ee69d2aa3a8705704f58d4a52748659f1
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-09 11:15:42 +0000

    updated

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-import json
-class Student:
-    def __init__(self, first_name, last_name, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-    def to_json(self, attrs=None):
-        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
-            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
-        return self.__dict__
+def greet_all(*names):
+    for name in names:
+        print(f"Hello, {name}")
+
+greet_all = ("Benjamin", "Joy", "Chemu")
