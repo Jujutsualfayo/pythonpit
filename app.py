3dfd9db4c784248fc63359ba1f51fa2175b8b771
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-09 07:47:09 +0000

    updated

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import json
-def class_to_json(obj):
-    return obj.__dict__
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self):
+        return self.__dict__
