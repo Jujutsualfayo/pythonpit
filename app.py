8fe2995068563f881b9b2fbfba2097e19168d861
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-09 10:29:31 +0000

    updated

--- app.py
+++ app.py
@@ -4,5 +4,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    def to_json(self):
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
         return self.__dict__
