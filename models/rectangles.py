a20734f031195f12fd8ee0005965bb5661444b1b
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-14 08:18:36 +0000

    updated

---
+++ models/rectangles.py
@@ -0,0 +1,50 @@
+from models.base import Base
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().self.id = id
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Value must be an integer")
+        if value <= 0:
+            raise ValueError("Value must be more than 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Value must be integer")
+        if value <= 0:
+            raise ValueError("Value must be more than 0")
+        self.__height = value
+    @property
+    def x(self):
+        return self.__x
+    @x.setter
+    def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Value must be an integer")
+        if value < 0:
+            raise ValueError("Value must be more than zero")
+        self.__x = value
+    @property
+    def y(self):
+        return self.__y
+    @y.setter
+    def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Value must be more than 0")
+        if value < 0:
+            raise ValueError("Value must be more than 0")
+        self.__y = value
+
