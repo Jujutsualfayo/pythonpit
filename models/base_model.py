c83d28d7f9c5fcc61e50bb7303bc0089ff9f6137
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-15 11:31:29 +0000

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -7,4 +7,5 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = self.created_at
     def __str__(self):
-        
+        '''Return the f string representation'''
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
