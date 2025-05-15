97e79b186a472a39e4abecd0857ebdaac15d5ff3
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-15 08:24:09 +0000

    updated

--- models/base.py
+++ models/base.py
@@ -12,5 +12,22 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file"""
+        filename = f"{cls.__name__}.json"
+
+        if list_objs is None:
+            list_dicts = []
+        else:
+            # convert each object to its dictionary form
+            list_dicts = [obj.to_dictionary() for obj in list_objs]
+
+        # convert to JSON string
+        json_string = cls.to_json_string(list_dicts)
+
+        # write to file
+        with open(filename, "w") as f:
+            f.write(json_string)
             
 
