a600bae41048b15f0f4c0042bbb84e59587286e6
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-15 11:33:07 +0000

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
+    '''Initialize the bmodel with a unique ID and timestamps'''
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
