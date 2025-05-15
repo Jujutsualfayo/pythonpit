4bcd1321f4b90fc645aae18817b0a1385f02e17a
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-15 08:08:58 +0000

    updatedd

--- models/base.py
+++ models/base.py
@@ -6,6 +6,7 @@ class Base:
             self.id = id
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    @staticmethod
     def to_json_string(list_dictionaries):
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
