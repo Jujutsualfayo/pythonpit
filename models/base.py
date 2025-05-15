4f51065126b048b1558cb3c8023e239ce94ab42a
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-15 08:07:49 +0000

    updated

--- models/base.py
+++ models/base.py
@@ -1,3 +1,4 @@
+import json
 class Base:
     __nb_objects = 0
     def __init__(self, id=None):
@@ -5,4 +6,10 @@ class Base:
             self.id = id
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+            
 
