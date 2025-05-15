44396212bc27a9e07f3d2f8d0bd2c65be5f49b4d
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-15 13:12:47 +0000

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -7,4 +7,18 @@ class BaseModel:
       self.id = uuid.uuid4()
       self.created_at = datetime.now()
       self.updated_at = self.created_at
+
+      """return the f string representation of the class"""
+      def __str__(self):
+         return f"[{self.__class.__name__}] ({self.id}) {self.__dict__}"
+
+      """updates the public instance attribute updated_at with the current datetime"""
+      def save(self):
+         self.updated_at = datetime.now()
+
+         """returns a dictionary containing all keys/values of __dict__ of the instance"""
+      def to_dict(self):
+         result = self.__dict__.copy()
+         result["created_at"] = self.created_at.isoformat()
+      
     
