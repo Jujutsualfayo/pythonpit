40b5ba52360c756be0c13fc02a4e2bfcb45f193a
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-15 12:31:18 +0000

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -2,27 +2,26 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    '''Initialize the bmodel with a unique ID and timestamps'''
+    """Initialize func and also derive the datetime instance"""
     def __init__(self):
-        self.id = str(uuid.uuid4())
+        self.id = uuid.uuid4()
         self.created_at = datetime.now()
         self.updated_at = self.created_at
+    """return f string representation of the instance """
     def __str__(self):
-        '''Return the f string representation'''
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__} "
+    
+    """updates the public instance attribute updated_at with the current datetime"""
     def save(self):
-        '''Updates the updated_at to current dattime'''
         self.updated_at = datetime.now()
+    """returns a dictionary containing all keys/values of __dict__ of the instance"""
     def to_dict(self):
-        """Return dattime objectsin ISO format also return copy of the instances"""
-        
-        #create a copy of the results instance
-        result = self.__dict__.copy()
-        #Convert datetime to iso format strings
+        result = self.__dict__.copy
         result["created_at"] = self.created_at.isoformat()
         result["updated_at"] = self.updated_at.isoformat()
-        #Add the classname
         result["__class__"] = self.__class__.__name__
 
         return result
-    
+
+
+
