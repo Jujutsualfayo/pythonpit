544a3b492e286d0aae8dac7add03659ba6c86a7e
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-16 09:42:51 +0000

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,11 @@
 import uuid
 from datetime import datetime
-
 class BaseModel:
-   """Initialise the base model and create unique timestamps"""
-   def __init__(self):
-      self.id = uuid.uuid4()
-      self.created_at = datetime.now()
-      self.updated_at = self.created_at
+    '''init and create unique id and timestamps for the func'''
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        
 
-      """return the f string representation of the class"""
-      def __str__(self):
-         return f"[{self.__class.__name__}] ({self.id}) {self.__dict__}"
-
-      """updates the public instance attribute updated_at with the current datetime"""
-      def save(self):
-         self.updated_at = datetime.now()
-
-         """returns a dictionary containing all keys/values of __dict__ of the instance"""
-      def to_dict(self):
-         result = self.__dict__.copy()
-         result["created_at"] = self.created_at.isoformat()
-         result["updated_at"] = self.updated_at.isoformat()
-         result["__class__"] = self.__class__.__name__
-         return result
 
 
       
