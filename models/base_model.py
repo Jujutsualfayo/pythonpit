ddfd6cadeabfe210acfada10108e2cafd1a261db
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-16 09:58:38 +0000

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,9 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
-    '''
+    '''create an f str formation of the my BaseModel'''
+    def __str__(self):
+        
    
 
 
