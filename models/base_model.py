941268c1718a165f0c4f908d962ae385a2ebaabf
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-16 09:44:58 +0000

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,10 @@ class BaseModel:
     '''init and create unique id and timestamps for the func'''
     def __init__(self):
         self.id = str(uuid.uuid4())
-        
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+   
+
 
 
 
