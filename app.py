a74a74087db817aeae4583a3b592614f5ec44f1b
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-21 09:01:50 +0000

    updated

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-def add(a, b):
-    return a + b
+class Student:
+    def __init__(self, name, age, cohort, dorm):
+        self.name = name
+        self.age = age
+        self.cohort = cohort
+        self.dorm = dorm
+student_1 = Student("Francis Nyatundo", 24, 22, "Aggrey house")
+print(student_1.name)
