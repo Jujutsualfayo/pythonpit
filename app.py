e92baae99aef2147107ad0a1c2673452ee122988
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-22 12:46:05 +0000

    updated

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 import cmd
+
 class HelloWorld(cmd.Cmd):
-    def greet(self, line):
-        print("Hello Benjamin")
-    def EoF(self, line):
+    """Simple command processor example."""
+    
+    def do_greet(self, person):
+        """greet [person]
+        Greet the named person"""
+        if person:
+            print ("hi,"), person
+        else:
+            print ('hi')
+    
+    def do_EOF(self, line):
         return True
-if __name__ == "__main__":
+    
+    def postloop(self):
+        print
+
+if __name__ == '__main__':
     HelloWorld().cmdloop()
