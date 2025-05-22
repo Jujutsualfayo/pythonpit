a7ed5dc5d91b2e751536173fe9aa1ace524b3258
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-22 13:07:23 +0000

    updated

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
 import cmd
 
-class HelloWorld(cmd.Cmd):
-    """Simple command processor example."""
-    
-    def do_greet(self, person):
-        """greet [person]
-        Greet the named person"""
-        if person:
-            print ("hi,"), person
-        else:
-            print ('hi')
+class InteractiveOrCommandLine(cmd.Cmd):
+    """Accepts commands via the normal interactive prompt or on the command line."""
+
+    def do_greet(self, line):
+        print 'hello,', line
     
     def do_EOF(self, line):
         return True
-    
-    def postloop(self):
-        print
 
 if __name__ == '__main__':
-    HelloWorld().cmdloop()
+    import sys
+    if len(sys.argv) > 1:
+        InteractiveOrCommandLine().onecmd(' '.join(sys.argv[1:]))
+    else:
+        InteractiveOrCommandLine().cmdloop()
