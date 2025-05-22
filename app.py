814ea80ef4d427efefa2dd06e4b40bebea1c4d19
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-22 10:59:15 +0000

    updated

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ class Student:
 
     def __str__(self):
         return
-    f"Name: {self.name}\nAge: {self.age}"
-    f"Cohort: {self.cohort}\nDorm: {self.cohort}"
+        f"Name: {self.name}\nAge: {self.age}"
+        f"Cohort: {self.cohort}\nDorm: {self.cohort}"
 student_1 = Student("Francis Nyatundo", 24, 22, "Aggrey house")
 print(student_1.name)
