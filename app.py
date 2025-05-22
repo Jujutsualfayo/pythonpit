24b0369abcb9f5b4a7f831efd3bbff9abbf58ea2
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-22 11:26:11 +0000

    updated

--- app.py
+++ app.py
@@ -11,11 +11,14 @@ class Student:
             f"Cohort: {self.cohort}\nGrade: {self.grade}"
         )
     def grading_hist(self, your_grad, option="Next class"):
-        if self.grade == grading_hist:
+        self.grade = 75
+        if self.grade > your_grad:
+            print(" You have not met the threshhold mark")
+
         return(
            print(f"Hello {self.name}\nYour score is {your_grad}\n"), 
            print(f"You pass to the {option}")
         )
-student_1 = Student("Francis Nyatundo", 24, 22, "Aggrey house")
+student_1 = Student("Francis Nyatundo", 24, 22, 85)
 student_1.grading_hist(90)
 print(student_1.grading_hist)
