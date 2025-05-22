2b44a840dd0e2d91ab286dd8b393cc8b4b6120f4
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-22 11:19:28 +0000

    updated

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 class Student:
-    def __init__(self, name, age, cohort, dorm):
+    def __init__(self, name, age, cohort, grade):
         self.name = name
         self.age = age
         self.cohort = cohort
-        self.dorm = dorm
+        self.grade = grade
 
     def __str__(self):
         return(
             f"Name: {self.name}\nAge: {self.age}"
-            f"Cohort: {self.cohort}\nDorm: {self.cohort}"
+            f"Cohort: {self.cohort}\nGrade: {self.grade}"
+        )
+    def grading_hist(self, your_grad, option="Next class"):
+        if self.grade == grading_hist:
+        return(
+           print(f"Hello {self.name}\nYour score is {your_grad}\n"), 
+           print(f"You pass to the {option}")
         )
 student_1 = Student("Francis Nyatundo", 24, 22, "Aggrey house")
-print(student_1.name)
+student_1.grading_hist(90)
+print(student_1.grading_hist)
