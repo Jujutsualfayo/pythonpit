0d1b35e0e49f9d726efbc8d0e8b769e948943fc0
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-24 09:34:48 +0000

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,10 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):# str repr of uuidand updated timestamps
+    def __init__(self, *args, **kwargs):# str repr of uuidand updated timestamps, adds features for adding key word arguments
+        if kwargs:
+            for key, value in kwargs.items()
+            
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
