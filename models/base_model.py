9546c647c466fca7957518a6449ab86fee84c28e
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-24 07:08:24 +0000

    updated str representation

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,12 @@ class BaseModel: #initialize and assign unique id and upd with current datetimes
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
-        
+    def __str__(self): # Brings abt the str representationof the dictionary
+        return (
+            f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        )
+    
+
 
 
 
