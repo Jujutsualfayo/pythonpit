9638e0b1d7573dbd08be8636f2a4732c4f902df9
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-24 07:41:09 +0000

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,11 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
+    def __str__(self): # returns the string representation of the dictionary
+        return (
+            f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        )
+    
 
 
 
