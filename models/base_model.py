cd3f23b0d3d92360f3ffae6f59e73eb0558e96cc
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-24 09:43:31 +0000

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -4,11 +4,15 @@ from datetime import datetime
 class BaseModel:
     def __init__(self, *args, **kwargs):# str repr of uuidand updated timestamps, adds features for adding key word arguments
         if kwargs:
-            for key, value in kwargs.items()
-            
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat)
+                elif key != "__class__":
+                    setattr(self, key, value)
+                else:
+                    self.id = str(uuid.uuid4())
+                    self.created_at = datetime.now()
+                    self.updated_at = self.created_at
     def __str__(self): # returns the string representation of the dictionary
         return (
             f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
