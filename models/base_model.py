a6e600e969b9771899ccbbb1d2a219a7d872a2e3
Benjamin Alfayo Otieno. <153228413+Jujutsualfayo@users.noreply.github.com>
2025-05-24 07:38:36 +0000

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,11 @@
 import uuid
 from datetime import datetime
 
-class BaseModel: #initialize and assign unique id and upd with current datetimes
-    def __init__(self):
+class BaseModel:
+    def __init__(self):# str repr of uuidand updated timestamps
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
-    def __str__(self): # Brings abt the str representationof the dictionary
-        return (
-            f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-        )
-    def save(self): # updates the  updated at with the current datetime
-        self.updated_at = datetime.now()
-    def to_dict(self): # returns a dictionary containing values of the dict instance
-        obj_dict = self.__dict__
-        obj_dict["__class__"] = self.__class__.__name__
-        obj_dict["created_at"] = self.created_at.isoformat()
-        obj_dict["updated_at"] = self.updated_at.isoformat()
-        return obj_dict
-
 
 
 
